f120740e094fbec2858e6fe06356e94a6d1d17ec
bigtimer-dev <aljnoma@gmail.com>
2025-10-23 21:49:24 -0400

    getting 60 fps

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +17,9 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
