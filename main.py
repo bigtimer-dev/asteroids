711022a0736b1b142b262d3f4b1ed76d36fb58ea
bigtimer-dev <aljnoma@gmail.com>
2025-10-24 10:57:10 -0400

    working turn directions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+import player
 
 
 def main():
@@ -8,7 +9,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player_triangle = player.Player(x, y)
     dt = 0
 
     while True:
@@ -16,10 +19,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player_triangle.draw(screen)
+        dt = clock.tick(60) / 1000
+        player_triangle.update(dt)
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick(60) / 1000
-        print(dt)
 
 
 if __name__ == "__main__":
